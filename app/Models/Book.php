08fd713ee93c645d37c6d3b385e9742a774f142e
dhias <dhiasa01@gmail.com>
2024-11-13 12:12:34 +0700

    membuat crud

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
-    //
+    public function bookshelf(): BelongsTo{
+        return $this->belongsTo(Bookshelf::class);
+    }
 }
